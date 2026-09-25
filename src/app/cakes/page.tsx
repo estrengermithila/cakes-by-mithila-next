8cfa000e8d2996cb220a439a97d1fd1c968f595d
estrengermithila <mithilafarjanam20@gmail.com>
2026-09-26 02:40:38 +0600

    fix navbar

---
+++ src/app/cakes/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllCakes = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AllCakes;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       data-theme='light'
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full  flex flex-col">
         
-        <div>
+        <div className="w-full max-w-[1440px] mx-auto">
           <Navbar></Navbar>
           {children}
         </div>
